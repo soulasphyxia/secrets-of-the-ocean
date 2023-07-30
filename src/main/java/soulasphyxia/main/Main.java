39c2cf3e92c0d06d347ab72764cc29c5a21854c5
soulasphyxia <maslyuk03@bk.ru>
2023-07-30 14:37:41 +0700

    commit

--- src/main/java/soulasphyxia/main/Main.java
+++ src/main/java/soulasphyxia/main/Main.java
@@ -1,9 +1,13 @@
 package soulasphyxia.main;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 import java.awt.*;
 import java.io.IOException;
 
+
 public class Main {
     public static void main(String[] args) throws IOException, FontFormatException {
+        new TypeReference<>(){};
         new Game();
     }
 }
